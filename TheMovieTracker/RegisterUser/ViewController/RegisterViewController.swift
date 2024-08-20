ae721f5537361f9fd13e5318bdfdcbb5e7c49bc8
vierrra <vierrra@hotmail.com>
2024-08-19 21:19:59 -0300

    insert validation screen register

--- TheMovieTracker/RegisterUser/ViewController/RegisterViewController.swift
+++ TheMovieTracker/RegisterUser/ViewController/RegisterViewController.swift
@@ -1,10 +1,16 @@
 //
+//  LoginViewController.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Renato Vieira on 18/08/24.
+//
 
 import UIKit
 
 class RegisterViewController: UIViewController {
     
     private var screen: RegisterScreen?
+    private var viewModel: RegisterViewModel? = RegisterViewModel()
     
     override func loadView() {
         screen = RegisterScreen()
@@ -14,7 +20,17 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.configProtocols()
+        self.isEnabledLoginButton(isEnable: false)
+    }
+    
+    private func configProtocols() {
         screen?.delegates(self, delegatesTextFields: self)
+    }
+    
+    private  func isEnabledLoginButton(isEnable: Bool) {
+        screen?.registerButton.isEnabled = isEnable
+        screen?.registerButton.backgroundColor = isEnable ? .colorLabel : .lightGray
     }
 }
 
@@ -26,5 +42,18 @@ extension RegisterViewController: RegisterScreenProtocol {
 }
 
 extension RegisterViewController: UITextFieldDelegate {
-    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text as? NSString {
+          let newText = text.replacingCharacters(in: range, with: string)
+          textField.text = newText
+            if let viewModel = viewModel, let screen = screen {
+                if viewModel.validationsFiledsTextFields(screen.emailTextField.text ?? "", screen.passwordTextField.text ?? "", confirmPassword: screen.confirmPasswordTextField.text ?? "") {
+                    isEnabledLoginButton(isEnable: true)
+              } else {
+                    isEnabledLoginButton(isEnable: false)
+              }
+            }
+        }
+        return false
+    }
 }
---
+++ TheMovieTracker/RegisterUser/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  RegisterViewModel.swift
+//  TheMovieTracker
+//
+//  Created by Renato Vieira on 19/08/24.
+//
+
+import UIKit
+
+class RegisterViewModel {
+    
+    private func isValidEmail(_ email: String) -> Bool {
+        let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailPattern)
+        return emailPred.evaluate(with: email)
+    }
+    
+    private func isValidPassword(_ password: String) -> Bool {
+        let passwordPattern = ".{6,}"
+        let passwordPred = NSPredicate(format: "SELF MATCHES %@", passwordPattern)
+        return passwordPred.evaluate(with: password)
+    }
+    
+    public func validationsFiledsTextFields(_ email: String, _ password: String, confirmPassword: String) -> Bool {
+        if isValidEmail(email) && isValidPassword(password) && isValidPassword(confirmPassword) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public func validateEqualsPassword(_ password: UITextField, _ confirmPasswordTextField: UITextField ) -> Bool {
+        if password.text == confirmPasswordTextField.text {
+            return true
+        } else {
+            return false
+        }
+    }
+}
