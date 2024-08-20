836cef0de916bdd8a5922d1dc72ebe1ee1717ba1
vierrra <vierrra@hotmail.com>
2024-08-19 21:12:29 -0300

    iInsert validation screen login

--- TheMovieTracker/Login/ViewController/LoginViewController.swift
+++ TheMovieTracker/Login/ViewController/LoginViewController.swift
@@ -1,10 +1,16 @@
 //
+//  LoginViewController.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Renato Vieira on 18/08/24.
+//
 
 import UIKit
 
 class LoginViewController: UIViewController {
     
     private var screen: LoginScreen?
+    private var viewModel: LoginViewModel? = LoginViewModel()
     
     override func loadView() {
         screen = LoginScreen()
@@ -14,7 +20,17 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.configProtocols()
+        self.isEnabledLoginButton(isEnable: false)
+    }
+    
+    private func configProtocols() {
         screen?.delegates(self, self)
+    }
+    
+    private  func isEnabledLoginButton(isEnable: Bool) {
+        screen?.loginButton.isEnabled = isEnable
+        screen?.loginButton.backgroundColor = isEnable ? .colorLabel : .lightGray
     }
 }
 
@@ -36,5 +52,19 @@ extension LoginViewController: LoginScreenProtocol {
 
 extension LoginViewController: UITextFieldDelegate {
     
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text as? NSString {
+          let newText = text.replacingCharacters(in: range, with: string)
+          textField.text = newText
+            if let viewModel = viewModel, let screen = screen {
+                if viewModel.validationsFiledsTextFields(screen.emailTextField.text ?? "", screen.passwordTextField.text ?? "") {
+                    isEnabledLoginButton(isEnable: true)
+              } else {
+                    isEnabledLoginButton(isEnable: false)
+              }
+            }
+        }
+        return false
+    }
 }
 
