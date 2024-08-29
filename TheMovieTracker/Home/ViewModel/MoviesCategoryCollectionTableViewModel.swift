558e51b57db510fb6aab53ed2e2510cb1626197f
vierrra <vierrra@hotmail.com>
2024-08-29 14:55:21 -0300

    Added methods viewModel

---
+++ TheMovieTracker/Home/ViewModel/MoviesCategoryCollectionTableViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesCategoryCollectionTableViewModel.swift
+//  TheMovieTracker
+//
+//  Created by Renato Vieira on 29/08/24.
+//
+
+import UIKit
+
+class MoviesCategoryCollectionTableViewModel: NSObject {
+
+}
